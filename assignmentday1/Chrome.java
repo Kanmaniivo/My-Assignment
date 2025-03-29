34078f90c5fe383aae0b0585a9c2a075e190dd39
Kanmaniivo <kanmaniivo3017@gmail.com>
2025-03-29 23:18:47 +0530

    Add files via upload

---
+++ assignmentday1/Chrome.java
@@ -0,0 +1,32 @@
+package assignmentday1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		//float
+		float version=91.0f;
+		System.out.println("version:" +version);
+
+		//String
+		String developer="Google";
+		System.out.println("developer:" +developer);
+
+		//boolean
+		boolean isBeta=false;
+		System.out.println("is Beta:" +isBeta);
+
+		//int
+		int releaseYear=2008;
+		System.out.println("release Year:" +releaseYear);
+
+		//char
+		char shortcutkey='C';
+		System.out.println("Shortcut Key:" +shortcutkey);
+
+
+	}
+
+}
